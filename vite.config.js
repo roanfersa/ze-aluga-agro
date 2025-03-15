8da3fd8e3be59e6026d4a5bb81a82f89d0b89123
Rodrigo Thenopholo <thenopholo92@icloud.com>
2025-03-15 08:36:46 -0300

    fix: corrigi inicialização do npm run dev

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      'react-is': path.resolve(__dirname, './node_modules/react-is/'),
+      './cjs/react-is.development.js': path.resolve(__dirname, './node_modules/react-is/cjs/react-is.production.min.js')
+    }
+  },
   build: {
     outDir: 'dist'
   }
